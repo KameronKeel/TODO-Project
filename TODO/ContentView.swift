077acff4f09fb5ff73cfcad012df6adc06430742
Kameron Kkeel <Kkeel3@live.spcollege.edu>
2020-08-27 15:39:39 -0400

    Initial Commit

---
+++ TODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODO
+//
+//  Created by Kameron Keel on 8/27/20.
+//  Copyright © 2020 Kameron Keel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
