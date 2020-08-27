13c151054f07f019c057f5a0c4f936fe50ca07e9
Kameron Kkeel <Kkeel3@live.spcollege.edu>
2020-08-27 16:19:52 -0400

    First commit to GitHub (partially done chapter 9)

--- TODO/ContentView.swift
+++ TODO/ContentView.swift
@@ -10,7 +10,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            
+        List{
+            
+            Section(header: Text("High Priority")){
+                
+                Group{
+            Text("Walk The Dog")
+            Text("Brush my teeth")
+            Text("Learn iOS development")
+            Text("Make Dinner")
+            Text("Do Laundry")
+            Text("Pay Bills")
+                }
+                
+                Group{
+            Text("Finish Homework")
+            Text("Change internet provider")
+            Text("Read Raywenderlich.com")
+            Text("Clean the kitchen")
+            Text("Wash the car")
+                }
+            }
+            
+            Section(header: Text("Low Priority")){
+            Text("Soccer practice")
+            Text("Eat ice cream")
+            Text("Take vocal lessons")
+            Text("Record hit single")
+            Text("Learn every martial art")
+            Text("Design Custome")
+            Text("Design crime-fighting vehicle")
+            Text("Come up with super hero name")
+            Text("Befriend the space raccoon")
+            Text("Save the world")
+                
+            }
+        }
+        .listStyle(GroupedListStyle())
+        .navigationBarTitle("Checklist")
     }
 }
 
@@ -18,4 +57,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
